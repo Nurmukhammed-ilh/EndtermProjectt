ec96d66ca6c98d3e7c70066e183af62204a3ceed
201348 <79683078+201348@users.noreply.github.com>
2021-03-07 22:22:27 +0600

    Update CrimeController.java

--- src/com/company/controllers/CrimeController.java
+++ src/com/company/controllers/CrimeController.java
@@ -15,7 +15,7 @@ public class CrimeController<wanted> {
         this.repo=repo;
     }
 
-    public String addCrime(int CriId, String CriName, String article, int CriAge, boolean wanted){
+    public String addCrime(int CriId, String CriName, String article, int CriAge, boolean wanted){ //method to add a new criminal
         Criminals criminals = new Criminals(CriId,CriName,article,CriAge,wanted);
         boolean add = repo.addCriminal(criminals);
         if (add == true) {
@@ -24,7 +24,7 @@ public class CrimeController<wanted> {
             return "Criminal was not added! Something wend wrong";
     }
 
-    public String getCrimeById(int id) {
+    public String getCrimeById(int id) { //display a criminal by his id
         Criminals criminals=repo.getCriminalId(id);
         if (criminals != null) {
             return criminals.toString();
@@ -32,15 +32,15 @@ public class CrimeController<wanted> {
             return "Criminals was not found!";
     }
 
-    public String getCrimeByCri(String article){
+    public String getCrimeByCri(String article){ //display a criminals by their articles(cases)
         List<Criminals> list = repo.getAllCriCri(article);
         return (list.size() == 0 ? "Criminals were not found!" : list.toString());
     }
-    public String getAllCriminal(){
+    public String getAllCriminal(){ //display a list of all criminals
         List<Criminals> list = repo.getAllCriminal();
         return (list.size() == 0 ? "Criminals were not found!" : list.toString());
     }
-    public String crichange(int id, boolean wanted) {
+    public String crichange(int id, boolean wanted) { //method to change criminals status(wanted or not)
         boolean change=repo.crichange(id,wanted);
         if (change != false) {
             return "New wanted status was added";
@@ -48,7 +48,7 @@ public class CrimeController<wanted> {
             return "Criminal was not found!";
     }
 
-    public String criage(int age){
+    public String criage(int age){ //method for finding criminals, whose age higher than entered
         List<Criminals> list = repo.criage(age);
         return (list.size() == 0 ? "There are no criminals in the database older than that age!" : list.toString());
     }
