9647eef041e029f505b1dc4c85a7554aa5989d11
201348 <79683078+201348@users.noreply.github.com>
2021-03-07 22:35:01 +0600

    Update EmployeeController.java

--- src/com/company/controllers/EmployeeController.java
+++ src/com/company/controllers/EmployeeController.java
@@ -13,7 +13,7 @@ public class EmployeeController {
         this.repo = repo;
     }
 
-    public String addEmp(int id,String name, String surname, String gender, int age, int salary, String position, String dateAdoption, String dismissalDate) {
+    public String addEmp(int id,String name, String surname, String gender, int age, int salary, String position, String dateAdoption, String dismissalDate) { //method to add a new employee, if we have
         Employee employee = new Employee(id,name,surname,gender,age,salary,position,dateAdoption,dismissalDate);
         boolean add = repo.addEmployee(employee);
         if (add == true) {
@@ -21,28 +21,28 @@ public class EmployeeController {
         } else
             return "Employee was not added! Something wend wrong";
     }
-    public String getEmpById(int id) {
+    public String getEmpById(int id) { //method to display employee by his id
         Employee employee=repo.getEmployeeId(id);
         if (employee != null) {
             return employee.toString();
         } else
             return "Employee was not found! Seems like he does not work here";
     }
-    public String getEmpByPos(String position) {
+    public String getEmpByPos(String position) { //display employee by his position(profession)
         Employee employee=repo.getEmployeePos(position);
         if (employee != null) {
             return employee.toString();
         } else
             return "Employee was not found! Seems like he does not work here";
     }
-    public String disEmp(int id){
+    public String disEmp(int id){ //delete employee by his id
         boolean delete=repo.disEmp(id);
         if (delete==false) {
             return "Employee was not found! Seems like he does not work here";
         } else
             return "Employee was deleted";
     }
-    public String changeEmp(int cash,int id) {
+    public String changeEmp(int cash,int id) { //method for changing an employee's salary by entering his id 
         boolean change=repo.changeEmp(cash,id);
         if (change != false) {
             return "New salary was added";
@@ -50,22 +50,22 @@ public class EmployeeController {
             return "Employee was not found! Seems like he does not work here";
     }
 
-    public String promEmp(int id, String position) {
+    public String promEmp(int id, String position) { //method to employee promotion
         boolean change=repo.promEmp(id,position);
         if (change != false) {
             return "Employee was promoted";
         } else
             return "Employee was not found! Seems like he does not work here";
     }
-    public String getAllEmployee(){
+    public String getAllEmployee(){ //display a list of all employees
         List<Employee> list = repo.getAllEmployee();
         return (list.size() == 0 ? "Employers were not found!" : list.toString());
     }
-    public String getAllEmployeeRel(){
+    public String getAllEmployeeRel(){ //display those employees who are still working 
         List<Employee> list = repo.getAllEmployeeRel();
         return (list.size() == 0 ? "Employers were not found!" : list.toString());
     }
-    public String kickEmp(String dismissaldate,int id){
+    public String kickEmp(String dismissaldate,int id){ //dismissal of employee by id
         boolean delete=repo.kickEmp(dismissaldate,id);
         if (delete==false) {
             return "Employee was not found! Seems like he does not work here";
