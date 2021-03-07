3091e8bbd82dc6840b466f8939de5e27ca5ad472
201348 <79683078+201348@users.noreply.github.com>
2021-03-07 23:15:58 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,14 @@ import com.company.Application;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main { //creation class
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Application app = new Application();
         int d=0;
         System.out.println("Welcome to the FBI database! ");
         System.out.println("Please enter the password to gain access: ");
-        while (d<=3) {
+        while (d<=3) { //conditions when entering a password
             if(d==3){
                 System.out.println("Not Allowed!");
                 System.exit(0);
